e60ff39c598507a6dc4880ba143ea3f98b3c5a23
JoshuaBurleson <shojukempo@gmail.com>
2019-08-09 21:33:56 -0400

    Update with first 3 stretch goals completed

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 
 app.use(express.static('./great-idea-website'));
 
 app.get('/', (req, res) => {
     res.end('./index.html');
 });
+
+app.get('/stretch', (req, res) => {
+    res.sendFile(path.join(__dirname + '/stretch1.html'));
+});
+
+app.get('/vision', (req, res) => {
+    res.sendFile(path.join(__dirname + '/great-idea-website/vision.html'));
+})
 
 app.listen(4000, () => {
     console.log('Listening');
